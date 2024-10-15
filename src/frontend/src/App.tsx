47ebccf3561d1a3d790b75af7d2b3df0ff1cd0f5
Lost Dark <kienandrewcarreondizon@gmail.com>
2024-10-15 13:21:16 +0000

    react-query

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { QueryClient } from '@tanstack/react-query';
 
 function App() {
   const [greeting, setGreeting] = useState('');
